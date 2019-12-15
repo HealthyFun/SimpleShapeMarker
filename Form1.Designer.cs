93eb6318be072629d564db077b940b1a6bc1a3c1
HealthyFun <56773238+HealthyFun@users.noreply.github.com>
2019-12-15 13:43:02 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace SimpleShapeMarker
+{
+    partial class SimpleShapeMarker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SimpleShapeMarker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "SimpleShapeMarker";
+            this.Text = "SimpleShapeMarker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleShapeMarker
+{
+    public partial class SimpleShapeMarker : Form
+
+    {
+        public SimpleShapeMarker()
+        {
+            InitializeComponent();
+        
+            this.BackColor = Color.White;
+            Button button1 = new Button();
+            button1.Text = "Let's draw!";
+            button1.Location = new Point(110, 10);
+            button1.Size = new Size(70, 40);
+            button1.BackColor = Color.LightGray;
+            button1.Click += new System.EventHandler(button1_Click);
+
+            this.Controls.Add(button1);
+
+        }
+
+        void button1_Click(object o, EventArgs e)
+        {
+            DrawSomeShape();
+        }
+
+
+        void DrawSomeShape()
+        {
+            Graphics g = this.CreateGraphics();
+
+            Pen greenPen = new Pen(Color.Green, 3);
+            Pen redPen = new Pen(Color.Red, 15);
+
+            g.DrawLine(greenPen, 140, 170, 140, 230);
+            g.DrawRectangle(greenPen, 100, 60, 50, 60);
+            g.DrawPie(greenPen, 100, 100, 100, 60, 10, 25);
+            g.DrawPie(redPen, 300, 100, 150, 80, 10, 75);
+            g.DrawPie(redPen, 400, 150, 100, 60, 40, 85);
+            g.DrawEllipse(redPen, 200, 150, 80, 60);
+
+            g.Dispose();
+        }
+
+        static void Main()
+        {
+            Application.Run(new SimpleShapeMarker());
+        }
+
+        //SimpleShapeMarker
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
